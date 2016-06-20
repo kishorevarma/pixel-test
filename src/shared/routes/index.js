9a77bb8c7375d7c0a59ff7e405a4cff8ef51f1d6
Kishore Varma <kishorevarma.n@gmail.com>
2016-06-21 10:48:00 +1200

    modified gitignore

--- src/shared/routes/index.js
+++ src/shared/routes/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../containers/AppContainer';
-import MessageContainer from '../containers/LiquorContainer';
+import LiquorContainer from '../containers/LiquorContainer';
 
 const routes = (
   <Route path="/" component={App}>
-    <IndexRoute component={MessageContainer} />
+    <IndexRoute component={LiquorContainer} />
   </Route>
 );
 export default routes;
